0c12c508a76372dab0b8309fb58ee8c599abad82
alexromanov <al8x.romanov@gmail.com>
2020-01-07 17:42:50 +0200

    Add html page for rates

--- src/main/java/com/alexromanov/cuconv/controller/MoneyAppController.java
+++ src/main/java/com/alexromanov/cuconv/controller/MoneyAppController.java
@@ -1,19 +1,26 @@
 package com.alexromanov.cuconv.controller;
 
 import com.alexromanov.cuconv.entity.ConvertMoneyForm;
+import com.alexromanov.cuconv.entity.ExchangeRateResponse;
 import com.alexromanov.cuconv.service.MoneyConverterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class MoneyAppController {
+	private static final String EXCHANGE_RATE_API = "https://api.exchangeratesapi.io/latest";
+	@Autowired
+	private RestTemplate restTemplate;
+
     private final MoneyConverterService moneyConverterService;
 
     @GetMapping(value = "/")
@@ -32,4 +39,11 @@ public class MoneyAppController {
         model.addAttribute("converted", convertResult);
         return "result";
     }
+
+    @GetMapping("/rates/current")
+    public String getCurrentRates(Model model){
+    	ExchangeRateResponse exchangeRateResponse = restTemplate.getForObject(EXCHANGE_RATE_API, ExchangeRateResponse.class);
+    	model.addAttribute("currentRates", exchangeRateResponse);
+    	return "exchangerates";
+	}
 }
